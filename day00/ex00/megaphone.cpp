6ef87d6c1aeb1e945786e7c405e4603bd27f864e
DmitryZabrotsky <reezee512@gmail.com>
2018-12-11 20:42:30 +0200

    d00 ex00

--- day00/ex00/megaphone.cpp
+++ day00/ex00/megaphone.cpp
@@ -2,3 +2,33 @@
 // Created by Dmitry ZABROTSKY on 12/11/18.
 //
 
+#include <iostream>
+
+int main(int ac, char* av[])
+{
+	int i;
+	int j;
+	char c;
+
+	if (ac >= 2)
+	{
+		i = 1;
+		while (i < ac)
+		{
+			j = 0;
+			while (av[i][j])
+			{
+				c = av[i][j];
+				if (c >= 'a' && c <= 'z')
+					c = c - ' ';
+				std::cout << c;
+				j++;
+			}
+		i++;
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	return (0);
+}
