9068ef0325876e3aa67c6e3aa9409099bd0099ac
DmitryZabrotsky <reezee512@gmail.com>
2019-01-23 16:49:10 +0200

    d07 ex00

---
+++ day07/ex00/whatever.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+template <typename T>
+void swap(T &a, T &b)
+{
+	T c = a;
+	a = b;
+	b = c;
+};
+
+template <typename T>
+T min(T &a, T &b)
+{
+	if (a < b)
+		return a;
+	else
+		return b;
+};
+
+template <typename T>
+T max(T &a, T &b)
+{
+	if (a > b)
+		return a;
+	else
+		return b;
+};
+
+int main()
+{
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	return 0;
+}
